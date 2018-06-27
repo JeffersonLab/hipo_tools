8b381cf4496453edc682da02d1bcd6e78ab9f8ca
tylern4 <tylern@canisius.edu>
2018-06-27 10:37:43 -0400

    homebrew science tap is depricated

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  brew tap homebrew/science
-  brew install root6
-  brew install llvm
+  brew install root llvm
   cd hipo_tools
   mkdir build && cd build
   CXX=/usr/local/opt/llvm/bin/clang++ cmake .. -DCMAKE_INSTALL_PREFIX=install
