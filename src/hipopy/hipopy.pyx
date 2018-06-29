8d5931d950136eff2d04bb340f18113295924acc
Nicholas Tyler <tylern@canisius.edu>
2018-06-29 13:08:14 -0400

    Ints look good
    Will check on floats

--- src/hipopy/hipopy.pyx
+++ src/hipopy/hipopy.pyx
@@ -1,5 +1,6 @@
 # distutils: language = c++
 from libcpp.vector cimport vector
+from libcpp.string cimport string
 from libcpp cimport bool
 
 cdef extern from "utils.cpp":
@@ -32,13 +33,21 @@ cdef extern from "hipo/reader.h" namespace "hipo":
     cdef cppclass reader:
       reader() except +
       reader(bool) except +
+      vector[string] getDictionary()
       void open(char*)
       int getRecordCount()
       bool next()
       bool  isOpen()
       void  showInfo()
       node *getBranch[T](int, int)
-      #node *getBranch(char*,char*)
+      #node *getBranch[T](char*,char*)
+
+
+def str_to_char(name):
+  cdef bytes name_bytes = name.encode()
+  cdef char* c_name = name_bytes
+  return c_name
+
 
 cdef class int_node:
   cdef node[int]*c_node
@@ -69,29 +78,34 @@ cdef class float_node:
   def getLength(self):
     return self.c_node.getLength()
 
-
-
 cdef class hipo_reader:
   cdef reader*c_reader
   def __cinit__(self):
     self.c_reader = new reader(True)
 
-  def __cinit__(self, filename):
-    self.c_reader = new reader(True)
-    cdef bytes py_bytes = filename.encode()
-    cdef char* c_string = py_bytes
-    self.c_reader.open(c_string)
+  def __cinit__(self, bool randonAccess=True):
+    self.c_reader = new reader(randonAccess)
 
-  def open(self, filename):
-    cdef bytes py_bytes = filename.encode()
-    cdef char* c_string = py_bytes
-    self.c_reader.open(c_string)
+  def __cinit__(self, string filename):
+    self.c_reader = new reader(True)
+    cdef bytes filename_bytes = filename.encode()
+    cdef char* c_filename = filename_bytes
+    self.c_reader.open(c_filename)
+
+  def __cinit__(self, bytes filename):
+    self.c_reader = new reader(True)
+    self.c_reader.open(filename)
+
+  def open(self, string filename):
+    cdef bytes filename_bytes = filename.encode()
+    cdef char* c_filename = filename_bytes
+    self.c_reader.open(c_filename)
 
   def isOpen(self):
     return self.c_reader.isOpen()
 
   def showInfo(self):
-    return self.c_reader.showInfo()
+    self.c_reader.showInfo()
 
   def getRecordCount(self):
     return self.c_reader.getRecordCount()
@@ -99,16 +113,29 @@ cdef class hipo_reader:
   def next(self):
     return self.c_reader.next()
 
-  def getIntNode(self, group, item):
+  def getDictionary(self):
+    return self.c_reader.getDictionary()
+
+  #def getIntNode(self, group, item):
+  #  cdef node[int]*c_node
+  #  if(isinstance(group, basestring)):
+  #    group = str_to_char(group)
+  #    item = str_to_char(item)
+  #  c_node = self.c_reader.getBranch[int](group,item)
+  #  py_node = int_node()
+  #  py_node.setup(c_node)
+  #  return py_node
+
+  def getIntNode(self, int group, int item):
     cdef node[int]*c_node
-    c_node = self.c_reader.getBranch[int](group, item)
+    c_node = self.c_reader.getBranch[int](group,item)
     py_node = int_node()
     py_node.setup(c_node)
     return py_node
 
-  def getFloatNode(self, group, item):
+  def getFloatNode(self, int group, int item):
     cdef node[float]*c_node
-    c_node = self.c_reader.getBranch[float](group, item)
+    c_node = self.c_reader.getBranch[float](group,item)
     py_node = float_node()
     py_node.setup(c_node)
     return py_node
--- src/hipopy/test.py
+++ src/hipopy/test.py
@@ -1,17 +1,18 @@
 from hipopy import hipo_reader
 import numpy as np
 
-reader = hipo_reader("/Users/tylern/Data/hipo/out_clas_004203.evio.101.hipo")
+reader = hipo_reader(b'/Users/tylern/Data/hipo/out_clas_004203.evio.101.hipo')
 
 rec_part_pid = reader.getIntNode(331, 1)  # REC::Particle pid
 rec_part_px = reader.getFloatNode(331, 3)  # REC::Particle px
 
+
 px = []
 num = 0
-while(reader.next()):
-    if(rec_part_pid.getValue(0) == 11):
-        px.append(rec_part_px.getValue(0))
+while(reader.next() and num < 5):
+    num += 1
+    print("length (" + str(rec_part_pid.getLength()) + "):", end=' ')
+    for i in range(0, rec_part_pid.getLength()):
+        print(rec_part_pid.getValue(i), end=' ')
 
-
-px = np.array(px)
-print(px)
+    print()
