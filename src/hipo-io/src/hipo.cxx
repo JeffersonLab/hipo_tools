4596527ed9a65221c3fa30b64a119cb7bb4bebc5
Whitney Armstrong <warmstrong@anl.gov>
2018-07-03 21:33:15 -0500

    Added options for auto code generation. 	modified:   hipo.cxx

--- src/hipo-io/src/hipo.cxx
+++ src/hipo-io/src/hipo.cxx
@@ -43,15 +43,29 @@ int main(int argc, char** argv) {
   int    N_events  = 0;
   bool   help      = false;
   bool   verbose   = false;
-  string out_banks   = "";
+  std::vector<string> out_banks;
+  string code_file_out   = "";
   bool   is_list_cmd = false;
   bool   is_dump_cmd = false;
+  bool   is_code_cmd = false;
   bool   is_help_cmd = false;
+  bool   all_banks = false;
 
+
+  auto code_cli = (
+    command("code").set(is_code_cmd) % "list the banks",
+    option("-c","--code") & value("codefile", code_file_out) % "output code file",
+    (
+      required("-b", "--banks") & values("banks",out_banks) % "banks which should be used." |
+      required("-a", "--all-banks").set(all_banks) % "all banks",
+      option("-i","--input")  & value("input file" , infile) 
+    )
+    );
 
   auto list_cli = (
     command("list").set(is_list_cmd) % "list the banks",
-    option("-b", "--bank") & value("banks",out_banks) % "banks which should be used."
+    required("-i","--input")  & value("input file" , infile) % "input file" ,
+    option("-b", "--bank") & values("banks",out_banks) % "banks which should be used."
     );
 
   auto dump_cli = (
@@ -67,8 +81,8 @@ int main(int argc, char** argv) {
     command("help").set(is_help_cmd) % "print help"
     );
 
-
   auto cli = (
+    code_cli |
     list_cli | 
     dump_cli | 
     help_cli | 
@@ -116,115 +130,137 @@ int main(int argc, char** argv) {
     }
   }
 
-  cout << "converting " << in_path  << "\n";
-  cout << "to         " << out_path << "\n";
+  //cout << "converting " << in_path  << "\n";
+  //cout << "to         " << out_path << "\n";
 
+  //char command[128];
+  //char filename[128];
+  //char bankname[128];
+  //bool doAll = false;
 
-    //char command[128];
-    //char filename[128];
-    //char bankname[128];
-    //bool doAll = false;
+  //std::vector<std::string>  banks;
+  //std::string str_command;
 
-    //std::vector<std::string>  banks;
-    //std::string str_command;
-
-    //if(argc>2){
-    //    sprintf( command,"%s",argv[1]);
-    //    sprintf( filename,"%s",argv[2]);
-    //    str_command = command;
-    //    if(argc>3){
-    //      sprintf(bankname,"%s",argv[3]);
-    //      std::string b_str(bankname);
-    //      if(b_str.compare("all")==0) doAll = true;
-    //      if(doAll==false){
-    //        int nbanks = argc;
-    //        for(int i = 3; i < nbanks; i++){
-    //          sprintf(bankname,"%s",argv[i]);
-    //          banks.push_back(std::string(bankname));
-    //        }
-    //      }
-    //    }
-    //} else {
-    //  printf(" \n\n Please provide a filename to read....\n\n");
-    //  exit(0);
-    //}
-
+  //if(argc>2){
+  //    sprintf( command,"%s",argv[1]);
+  //    sprintf( filename,"%s",argv[2]);
+  //    str_command = command;
+  //    if(argc>3){
+  //      sprintf(bankname,"%s",argv[3]);
+  //      std::string b_str(bankname);
+  //      if(b_str.compare("all")==0) doAll = true;
+  //      if(doAll==false){
+  //        int nbanks = argc;
+  //        for(int i = 3; i < nbanks; i++){
+  //          sprintf(bankname,"%s",argv[i]);
+  //          banks.push_back(std::string(bankname));
+  //        }
+  //      }
+  //    }
+  //} else {
+  //  printf(" \n\n Please provide a filename to read....\n\n");
+  //  exit(0);
+  //}
+  //for(int i = 0; i < out_banks.size(); i++){
+  //  if(dict->hasSchema(out_banks[i].c_str())==true){
+  //  }
+  //  }
 
   hipo::reader hip_reader;
   hip_reader.open(in_path.string().c_str());
-  //hip_reader.showInfo();
-  //hipo::dictionary *dict = hip_reader.getSchemaDictionary();
+  hip_reader.showInfo();
+  hipo::dictionary *dict = hip_reader.getSchemaDictionary();
 
-    //if(str_command.compare("--list")==0){
-    //  dict->ls();
-    //}
-
-    //if(str_command.compare("--dump")==0){
-    //  dict->ls(1);
-    //}
-
-    //if(str_command.compare("--code")==0){
-    //  printf("\n\n");
-
-    //  if(banks.size()==0&&doAll==false){
-    //    printf("[--code] error : please provide bank names..\n\n");
-    //  } else {
-    //    printf("[code] ----> starting code generation....\n");
-    //    printf("[open] ----> writing file : runFileLoop.cc\n");
-    //    ofstream my_cc_file ("runFileLoop.cc");
-    //    my_cc_file << hipo::utils::getFileHeader().c_str() << std::endl;
-
-    //    if(doAll==true){
-    //      banks.clear();
-    //      banks = dict->getSchemaList();
-    //    }
-
-    //    for(int i = 0; i < banks.size(); i++){
-    //      if(dict->hasSchema(banks[i].c_str())==true){
-    //        std::vector<std::string>  code = dict->getSchema(banks[i].c_str()).branchesCode();
-
-    //        for(int v = 0; v < code.size(); v++){
-    //          //printf("%s\n",code[v].c_str());
-    //          my_cc_file << code[v].c_str() << std::endl;
-    //        }
-    //      } else {
-    //        printf("// ** error ** can not find bank with name [%s]\n",banks[i].c_str());
-    //      }
-    //    }
-    //    std::vector<std::string>  access_code = dict->getSchema(banks[0].c_str()).branchesAccessCode();
-    //    //printf("%s\n",hipo::utils::getFileTrailer(access_code[0].c_str()).c_str());
-    //    my_cc_file << hipo::utils::getFileTrailer(access_code[0].c_str()).c_str() << std::endl;
-    //    my_cc_file.close();
-    //  }
-    //}
-
-
-    if(is_dump_cmd) {
-      hipo::writer hip_writer;
-      hip_writer.open(out_path.string().c_str());
-
-      hipo::record record;
-      hipo::event  event;
-
-      int nrecords = hip_reader.getRecordCount();
-      int ecounter = 0;
-
-      std::cout << "  N events : " << N_events << "\n";
-      std::cout << "  nrecords : " << nrecords << "\n";
-      while( hip_reader.next() ) {
-        if( (N_events>0) && (ecounter >= N_events) ) {
-          break;
+  if(is_list_cmd) {
+    if(out_banks.size() == 0){
+      dict->ls();
+    } else {
+      // get list of all the banks and search for the containing string
+      auto all_banks = dict->getSchemaList();
+      for(const auto& an_out_bank : out_banks ){
+        for(const auto& abank : all_banks ){
+          if (abank.find(an_out_bank) != std::string::npos) {
+            std::cout << abank << "\n";
+          }
         }
-
-        event.showInfo();
-        event = *(hip_reader.getEvent());
-        hip_writer.writeEvent(event);
-        //printf(" event # %d\n",ecounter);
-        ecounter++;
       }
-      hip_writer.close();
     }
+  }
+  //if(str_command.compare("--dump")==0){
+  //  dict->ls(1);
+  //}
+
+  if( is_code_cmd ) {
+    std::vector<std::string> code_nodes;
+    if(all_banks==true){
+      //banks.clear();
+      code_nodes = dict->getSchemaList();
+    } else {
+      // get list of all the banks and search for the containing string
+      auto all_banks = dict->getSchemaList();
+      for(const auto& an_out_bank : out_banks ){
+        for(const auto& abank : all_banks ){
+          if (abank.find(an_out_bank) != std::string::npos) {
+            code_nodes.push_back(abank);
+            //std::cout << abank << "\n";
+          }
+        }
+      }
+    }
+    //if(out_banks.size()==0){
+    //  printf("[--code] error : please provide bank names..\n\n");
+    //} else {
+      printf("[code] ----> starting code generation....\n");
+      printf("[open] ----> writing file : runFileLoop.cc\n");
+      ofstream my_cc_file ("runFileLoop.cc");
+      my_cc_file << hipo::utils::getFileHeader().c_str() << std::endl;
 
 
-    return 0;
+      for(int i = 0; i < code_nodes.size(); i++){
+        if(dict->hasSchema(code_nodes[i].c_str())==true){
+          std::vector<std::string>  code = dict->getSchema(code_nodes[i].c_str()).branchesCode();
+
+          for(int v = 0; v < code.size(); v++){
+            //printf("%s\n",code[v].c_str());
+            my_cc_file << code[v].c_str() << std::endl;
+          }
+        } else {
+          printf("// ** error ** can not find bank with name [%s]\n",code_nodes[i].c_str());
+        }
+      }
+      std::vector<std::string>  access_code = dict->getSchema(code_nodes[0].c_str()).branchesAccessCode();
+      //printf("%s\n",hipo::utils::getFileTrailer(access_code[0].c_str()).c_str());
+      my_cc_file << hipo::utils::getFileTrailer(access_code[0].c_str()).c_str() << std::endl;
+      my_cc_file.close();
+    //}
+  }
+
+  if(is_dump_cmd) {
+    hipo::writer hip_writer;
+    hip_writer.open(out_path.string().c_str());
+
+    hipo::record record;
+    hipo::event  event;
+
+    int nrecords = hip_reader.getRecordCount();
+    int ecounter = 0;
+
+    std::cout << "  N events : " << N_events << "\n";
+    std::cout << "  nrecords : " << nrecords << "\n";
+    while( hip_reader.next() ) {
+      if( (N_events>0) && (ecounter >= N_events) ) {
+        break;
+      }
+
+      event.showInfo();
+      event = *(hip_reader.getEvent());
+      hip_writer.writeEvent(event);
+      //printf(" event # %d\n",ecounter);
+      ecounter++;
+    }
+    hip_writer.close();
+  }
+
+
+  return 0;
 }
